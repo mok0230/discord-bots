b2acba98a41a76d184136305432b7c54a5935240
mok <mok0230@protonmail.com>
2021-11-24 10:39:59 -0800

    hello world bot can interact with messages

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 require('dotenv').config();
-const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Client({
+  intents: [ Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_INTEGRATIONS ]
+});
+
+client.on("ready", () => {
+  console.log('hello world', client.user.tag);
+})
+
+client.on("message", msg => {
+  if (msg.content === 'hello') {
+    msg.reply('world')
+  }
+})
 
 client.login(process.env.BOT_TOKEN);
